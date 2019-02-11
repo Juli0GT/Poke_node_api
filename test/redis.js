00c45db15f86542056e833b5b687fd21ab1801ee
Juli0GT <julio.gteran@gmail.com>
2019-02-12 00:04:53 +0100

    Add test for redis getter method 	- Should return false when pokemon data is not stored in cache. 	- Test is failing.

---
+++ test/redis.js
@@ -0,0 +1,39 @@
+const redisFunctions = require('../helpers/redis.js');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const should = chai.should();
+const redis = require('redis-mock');
+
+chai.use(chaiAsPromised);
+
+
+describe('Redis', () => {
+
+  describe('Redis getter', () => {
+    let client;
+    let requestedPokemon;
+
+    before(done => {
+      client = redis.createClient();
+      done();
+    })
+
+    describe('Pokemon is not stored', () => {
+      before(done => {
+        const path = '/pokemon/pikachu';
+        requestedPokemon = redisFunctions.get({
+          path,
+          client,
+        });    
+        done();
+      })
+
+      it('Should return false when pokemon data is not stored in cache', (done) => {
+        requestedPokemon.then(result => {
+          result.should.equal(false);
+        }).finally(done);
+      });
+
+    });
+  });
+});
