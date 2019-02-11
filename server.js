62b626689f7d6dd8d1b430102a580c73d4b6a786
Juli0GT <julio.gteran@gmail.com>
2019-02-11 23:39:34 +0100

    Add get '/pokemon/:name' route and calls external api 	- Gets data from https://pokeapi.co/api/v2 3rd party api. 	- Send 3rd party response as response for get '/pokemon/:name'

--- server.js
+++ server.js
@@ -1,7 +1,80 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json({
+  limit: '100k',
+}));
+
+
+const axiosInstance = axios.create({
+  baseURL: 'https://pokeapi.co/api/v2',
+  timeout: 1000,
+});
+
+app.listen(PORT);
+
+const returnHttpResponse = ({
+  res,
+  status,
+  payload
+}) => res.status(status).json(payload);
+
+
+const getPokemonFromAPI = ({
+  path,
+  res,
+}) => {
+  return axiosInstance.get(path)
+  .then(resp => {
+    const { data } = resp;
+    const status = 200;
+
+    const payload = {
+      success: true,
+      data,
+    };
+
+    returnHttpResponse({
+      res,
+      status,
+      payload,
+    });
+  })
+  .catch(err => {
+    const {
+      status,
+      statusText,
+    } = err.response;
+
+    const payload = {
+      success: false,
+      error: {
+        code: status,
+        message: statusText,
+      }
+    };
+
+    returnHttpResponse({
+      res,
+      status,
+      payload,
+    });
+  });
+}
+
+app.get('/pokemon/:name', (req, res) => {
+  const path = req.path;
+  getPokemonFromAPI({
+    path,
+    res,
+  });
+})
+
 
 console.log(`Runing at port: ${PORT}`);
 
